b316e826fb95245925dfa91463da0cb0e75a0775
informati <46190977+Matthieusz@users.noreply.github.com>
2025-08-05 16:40:04 +0200

    feat: add schemas to drizzle object

--- apps/server/src/db/index.ts
+++ apps/server/src/db/index.ts
@@ -1,3 +1,7 @@
 import { drizzle } from "drizzle-orm/node-postgres";
+import { auth } from "./schema/auth";
+import { todo } from "./schema/todo";
 
-export const db = drizzle(process.env.DATABASE_URL || "");
+export const db = drizzle(process.env.DATABASE_URL || "", {
+	schema: { ...auth, ...todo },
+});
