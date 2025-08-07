7c2c68899b852130ab5fdce4654d4fbdf9d40250
informati <46190977+Matthieusz@users.noreply.github.com>
2025-08-07 17:05:40 +0200

    feat: move dynamic breadcrumb logic to seperate component

---
+++ apps/web/src/components/breadcrumb-nav.tsx
@@ -0,0 +1,36 @@
+import { isMatch, Link, useMatches } from "@tanstack/react-router";
+import {
+	Breadcrumb,
+	BreadcrumbItem,
+	BreadcrumbList,
+	BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+
+export const BreadcrumbNav = () => {
+	const matches = useMatches();
+	const matchesWithCrumbs = matches.filter((match) =>
+		isMatch(match, "loaderData.crumb")
+	);
+
+	const items = matchesWithCrumbs.map(({ pathname, loaderData }) => {
+		return {
+			href: pathname,
+			label: loaderData?.crumb,
+		};
+	});
+
+	return (
+		<Breadcrumb>
+			<BreadcrumbList>
+				{items.map((item, index) => (
+					<BreadcrumbItem key={item.href}>
+						<Link className="breadcrumb-link" to={item.href}>
+							{item.label}
+						</Link>
+						{index < items.length - 1 && <BreadcrumbSeparator />}
+					</BreadcrumbItem>
+				))}
+			</BreadcrumbList>
+		</Breadcrumb>
+	);
+};
