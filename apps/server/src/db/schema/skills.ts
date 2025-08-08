e3f2b9e40b5719abe9ccbe8e8aa17ac68dadd202
informati <46190977+Matthieusz@users.noreply.github.com>
2025-08-08 11:27:12 +0200

    feat: add database schema for skills

---
+++ apps/server/src/db/schema/skills.ts
@@ -0,0 +1,26 @@
+import { integer, pgTable, serial, text } from "drizzle-orm/pg-core";
+
+export const range = pgTable("range", {
+	id: serial("id").primaryKey(),
+	level: integer("level").notNull(),
+	image: text("image"),
+	name: text("name").notNull(),
+});
+
+export const skills = pgTable("skills", {
+	id: serial("id").primaryKey(),
+	name: text("name").notNull(),
+	description: text("description").notNull(),
+	professionId: integer("profession_id")
+		.references(() => professions.id)
+		.notNull(),
+	rangeId: integer("range_id")
+		.references(() => range.id)
+		.notNull(),
+});
+
+export const professions = pgTable("professions", {
+	id: serial("id").primaryKey(),
+	name: text("name").notNull(),
+	description: text("description").notNull(),
+});
