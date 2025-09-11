59836afee844ef2ad55570981259d1b8e4f4c373
informati <46190977+Matthieusz@users.noreply.github.com>
2025-09-11 17:00:56 +0200

    feat: implement back button in NotFound component

--- apps/web/src/components/not-found.tsx
+++ apps/web/src/components/not-found.tsx
@@ -1,9 +1,20 @@
+import { Link } from "@tanstack/react-router";
 import FuzzyText from "./FuzzyText";
 
 export default function NotFound() {
 	return (
-		<div className="flex h-full w-full items-center justify-center text-wrap">
-			<FuzzyText baseIntensity={0.2}>404</FuzzyText>
+		<div className="flex h-full w-full flex-col items-center justify-center gap-2">
+			<FuzzyText baseIntensity={0.2} fontSize={158}>
+				404
+			</FuzzyText>
+			<FuzzyText baseIntensity={0.1} fontSize={30}>
+				Strona nie istnieje
+			</FuzzyText>
+			<Link className="mt-12" to="/dashboard">
+				<FuzzyText baseIntensity={0.1} fontSize={18}>
+					Wróć do głównej strony
+				</FuzzyText>
+			</Link>
 		</div>
 	);
 }
