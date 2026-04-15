a28fb0369463b140b52f4ce4d42386982d7359d9
informati <46190977+Matthieusz@users.noreply.github.com>
2026-04-15 12:06:48 +0200

    chore: remove spacing

--- packages/db/src/types.ts
+++ packages/db/src/types.ts
@@ -12,20 +12,14 @@ export type User = InferSelectModel<typeof user>;
 export type Session = InferSelectModel<typeof session>;
 export type Account = InferSelectModel<typeof account>;
 export type Verification = InferSelectModel<typeof verification>;
-
 export type AuctionSignup = InferSelectModel<typeof auctionSignups>;
-
 export type Announcement = InferSelectModel<typeof announcement>;
-
 export type Hero = InferSelectModel<typeof hero>;
 export type HeroBet = InferSelectModel<typeof heroBet>;
 export type HeroBetMember = InferSelectModel<typeof heroBetMember>;
 export type UserStats = InferSelectModel<typeof userStats>;
-
 export type Event = InferSelectModel<typeof event>;
-
 export type Range = InferSelectModel<typeof range>;
 export type Skill = InferSelectModel<typeof skills>;
 export type Profession = InferSelectModel<typeof professions>;
-
 export type Todo = InferSelectModel<typeof todo>;
